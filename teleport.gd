f785cb3f880cf2ce3bb492268f9cffd4df302817
rainot <olga24@onet.pl>
2025-11-15 05:08:05 +0100

    Add 2 level and fix water

--- teleport.gd
+++ teleport.gd
@@ -3,9 +3,7 @@ extends Area2D
 @export var output: Node2D;
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("ball"):
-		body.teleport(output.position)
+	body.teleport(output.position)
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.is_in_group("ball"):
-		body.teleported = false;
+	body.teleported = false;
