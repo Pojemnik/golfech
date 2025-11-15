a6d3bbdbb1888519e5c0af511b4a449b741f5ad7
Pojemnik <andrzej.gauza@outlook.com>
2025-11-15 01:45:42 +0100

    Add water field

--- ball.gd
+++ ball.gd
@@ -1,25 +1,35 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@export var spawnpoint: Node2D;
 
 var start_vector = null;
 var current_vector = Vector2.ZERO;
 var teleported = false;
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func respawn() -> void:
+	teleport(spawnpoint.position)
+	linear_velocity = Vector2.ZERO;
+	teleported = false;
+	
+func teleport(pos: Vector2):
+	if (!teleported):
+		teleported = true;
+		PhysicsServer2D.body_set_state(
+			get_rid(),
+			PhysicsServer2D.BODY_STATE_TRANSFORM,
+			Transform2D.IDENTITY.translated(pos)
+		);
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("lmb") and sleeping:
-		start_vector = get_viewport().get_mouse_position()
+		start_vector = get_viewport().get_mouse_position();
 	
 	if (start_vector):
-		current_vector = (get_viewport().get_mouse_position() - start_vector) / 3
-		$Line2D.points = [Vector2.ZERO, current_vector]
+		current_vector = (get_viewport().get_mouse_position() - start_vector) / 3;
+		$Line2D.points = [Vector2.ZERO, current_vector];
 	else:
-		$Line2D.points = []
+		$Line2D.points = [];
 	
 	if Input.is_action_just_released("lmb") and start_vector:
-		apply_impulse(-current_vector)
+		apply_impulse(-current_vector);
 		start_vector = null;
--- teleport.gd
+++ teleport.gd
@@ -3,14 +3,7 @@ extends Area2D
 @export var output: Node2D;
 
 func _on_body_entered(body: Node2D) -> void:
-	if (!body.teleported):
-		body.teleported = true;
-		PhysicsServer2D.body_set_state(
-			body.get_rid(),
-			PhysicsServer2D.BODY_STATE_TRANSFORM,
-			Transform2D.IDENTITY.translated(output.position)
-		);
-
+	body.teleport(output.position)
 
 func _on_body_exited(body: Node2D) -> void:
 	body.teleported = false;
